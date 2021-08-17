4895716bb8b7518d5996c507b9795eafce8ac8f7
Stéphah <stephaniarak77@gmail.com>
2021-08-17 10:05:44 +0300

    front

---
+++ test-form-php/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary nav">
+  <div class="container-fluid ma">
+    <div  class="row">
+        <div class="col-xs-3">
+          <a class="navbar-brand" href="./index.php">Accueil</a>
+        </div>
+        <div class="col-xs-9">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="./liste.php">Liste selon les catégories
+          </a>
+        </li>
+      </ul>
+  </div>
+  </div>
+</div>
+</nav>
